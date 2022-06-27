e0b9c7cf267a32d304a67642ad81b8be4f254245
Brandon Dague <Brandon.Dague@ibm.com>
2022-06-27 13:12:22 -0500

    Getting Started and challenge 1

---
+++ section-one/Challenge 1/YourInitials.java
@@ -0,0 +1,13 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        //print initials here.
+        System.out.println("BBBB    DDDD");
+        System.out.println("B   B   D   D");
+        System.out.println("B   B   D   D");
+        System.out.println("BBBB    D   D");
+        System.out.println("B   B   D   D");
+        System.out.println("B   B   D   D");
+        System.out.println("BBBB    DDDD");
+
+    }
+}
