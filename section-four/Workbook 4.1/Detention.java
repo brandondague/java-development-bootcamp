b5419c7582e5b2354b0bee57201602d704164dd3
Brandon Dague <Brandon.Dague@ibm.com>
2022-06-29 11:50:23 -0500

    Section 4 complete

---
+++ section-four/Workbook 4.1/Detention.java
@@ -0,0 +1,30 @@
+public class Detention {
+    public static void main(String[] args) {
+
+        
+
+        // Task 2 – call the function 6 times.
+        printLines(6);
+   }
+
+
+
+    //Task 1 - Make a function here. See the doc comment for details. 
+
+    /**    
+      * Funtion name: printLines
+      *
+      * Inside the function:
+      *   1. prints the four lines       
+      */
+    public static void printLines(int num) {
+      //what Bart did so far
+      for (int x = 0; x < num; x++) {
+        System.out.println("I will not copy and paste code.");
+        System.out.println("I will use Camel Case when writing class names.");
+        System.out.println("I will use lower Camel Case when writing function names.");
+        System.out.println("I will use lower Camel Case when writing variables names.\n");
+      }
+    }
+
+}
