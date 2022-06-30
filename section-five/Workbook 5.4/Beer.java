d48f905246f6c09dbfffbca9be9079b4a4982910
Brandon Dague <Brandon.Dague@ibm.com>
2022-06-30 10:02:21 -0500

    Section 5 complete

---
+++ section-five/Workbook 5.4/Beer.java
@@ -0,0 +1,24 @@
+public class Beer {
+    public static void main(String[] args) {
+       // Task 2: make a for loop that starts at i = 99, and finishes at i=1.
+       for (int x = 99; x > 0; x--) {
+         sing(x);
+       }
+    }
+
+    /** Task 1
+     * Function name – sing.
+     * @param i (int)
+     * 
+     * 
+     * Inside the function: 
+     *  1. print:
+     *      <i> bottles of beer on the wall, <i> bottles of beer!
+     *      take one down, pass it around, <i-1> bottles of beer on the wall!
+     */
+    public static void sing(int i) {
+        System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer!");
+        System.out.println("take one down, pass it around, " + (i - 1) + " bottles of beer on the wall!");
+    }
+
+}
